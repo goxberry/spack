c9d6b4a8bf4e079f54883083ea260f9f9f1c27a6
Geoffrey Malcolm Oxberry <goxberry@gmail.com>
2018-01-30 07:17:46 -0800

    WIP: libelf: actually build shared library on Darwin
    I don't think this effort is going to work, but in the event that
    cribbing from NixOS does actually lead to libelf shared libraries on
    darwin, here's the start of what's needed.

--- var/spack/repos/builtin/packages/libelf/package.py
+++ var/spack/repos/builtin/packages/libelf/package.py
@@ -23,6 +23,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import sys
+import inspect
 
 
 class Libelf(AutotoolsPackage):
@@ -39,10 +41,38 @@ class Libelf(AutotoolsPackage):
 
     provides('elf@0')
 
+    # To build a shared library, must run autoreconf
+    depends_on('m4', type='build', when='platform=darwin')
+    depends_on('autoconf', type='build', when='platform=darwin')
+    depends_on('automake', type='build', when='platform=darwin')
+    depends_on('libtool', type='build', when='platform=darwin')
+    force_autoreconf = (True if sys.platform == 'darwin' else False)
+
+    @when('platform=darwin')
+    def autoreconf(self, spec, prefix):
+        with working_dir(self.configure_directory):
+            m = inspect.getmodule(self)
+            # This line is what is needed most of the time
+            # --install, --verbose, --force
+            autoreconf_args = ['-ivf']
+            m.autoreconf(*autoreconf_args)
+
     def configure_args(self):
         args = ["--enable-shared",
                 "--disable-dependency-tracking",
                 "--disable-debug"]
+
+        # Flags courtesy of NixOS:
+        # https://git.mayflower.de/NixOS/nixpkgs/commit/e73d805aa9f7349cc32d5a89c20efb7a3f86eba4
+        if self.spec.satisfies('platform=darwin'):
+            # Configure check for dynamic lib support is broken, see
+            # http://lists.uclibc.org/pipermail/uclibc-cvs/2005-August/019383.html
+            # Passing this option requires running autoreconf
+            args.append('mr_cv_target_elf=yes')
+            # Libelf's custom NLS macros fail to determine the catalog file extension
+            # on Darwin, so disable NLS for now.
+            args.append('--disable-nls')
+
         return args
 
     def install(self, spec, prefix):
